3d51314513e233d786ea7d9a3017faa53a542036
Daniil-Danone <mr.super.ko@inbox.ru>
2024-12-08 22:59:12 +0600

    feat: completed block - matric arrays

---
+++ Двумерные массивы/Базовые операции/solutionA.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+
+
+using namespace std;
+
+int main() {
+    int n, m, a[100][100];
+
+    cin >> n >> m;
+
+    int rows[100];
+    int cols[100];
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++){
+            cin >> a[i][j];
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        int row = 0;
+        for (int j = 0; j < m; j++){
+            row += a[i][j];
+        }
+        cout << row << " ";
+    }
+
+    cout << endl;
+
+    for (int i = 0; i < m; i++) {
+        int col = 0;
+        for (int j = 0; j < n; j++){
+            col += a[j][i];
+        }
+        cout << col << " ";
+    }
+
+    cout << endl;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++){
+            cout << a[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
---
+++ Двумерные массивы/Базовые операции/solutionC.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+
+using namespace std;
+
+int main() {
+    int n, a[100][100];
+
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++){
+            cin >> a[i][j];
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++){
+            cout << a[n-j-1][n-i-1] << " ";
+        }
+
+        cout << endl;
+    }
+
+}
